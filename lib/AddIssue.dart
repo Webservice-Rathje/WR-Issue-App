9da6ec5c850a54d831ff8466850e8daf4c1010ac
MathisBurger <handymathis@gmail.com>
2021-01-11 09:32:30 +0100

    initial commit

---
+++ lib/AddIssue.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class AddIssue extends StatelessWidget {
+  AddIssue({
+    Key key,
+  }) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xffffffff),
+      body: Stack(
+        children: <Widget>[],
+      ),
+    );
+  }
+}
