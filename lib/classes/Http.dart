929a61605a9d6a34ec185a10271a8bc590468bd0
Nick Rathje <nick@rathje-reinsbuettel.de>
2021-01-12 21:23:39 +0100

    removed shit from MB

--- lib/classes/Http.dart
+++ lib/classes/Http.dart
@@ -4,38 +4,6 @@ import '../classes/FileUtils.dart';
 import 'package:http/http.dart' as http;
 
 
-class Fetcher {
-  String baseURL = "https://api.wr-issue.de/beta";
-
-  Future<void> generateKey() async {
-      String url = baseURL + "/v1/mobile/generateKey";
-      print(url);
-      var response = await http.get("https://github.com/MathisBurger/fussball_trainer_managing_app/blob/master/lib/AddingViaActionButton.dart");
-      print("after async");
-      print("Key: " + response.body);
-      //Map<String, dynamic> json = jsonDecode(response.toString());
-  }
-
-  Future<bool> checkInternetPerm() async {
-
-  }
-
-  Future<bool> checkConnection() async {
-    print("AAACTIVE");
-    try {
-      final result = await InternetAddress.lookup('google.com');
-      print("Älter und fetter");
-      if (result.isNotEmpty && result[0].rawAddress.isNotEmpty) {
-        print("yee buster");
-        return true;
-      }
-    } on SocketException catch (err) {
-      print(err);
-      print("fat fat");
-      return false;
-    }
-  }
-}
 class APICommunication {
   String baseUrl = "https://api.wr-issue.de/beta/";
   String APIVersion = "v1/";
--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,6 @@ import 'package:app/pages/home.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  new Fetcher().checkConnection().then((value) => print(value));
   runApp(MaterialApp(
     title: "WR-Issue",
     home: Home(),
